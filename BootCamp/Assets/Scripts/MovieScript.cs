e5d0c457a9ad4a82f105f78ee1cefc8c0327bb3f
MathiasKirkegaard <MathiasKirkegaard151090@gmail.com>
2014-05-05 09:49:07 +0200

    <AA_movie>

---
+++ BootCamp/Assets/Scripts/MovieScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovieScript : MonoBehaviour {
+
+	int buttonSize 		= 200;
+	int buttonHeight	= 100;
+
+	string projectPath;
+	
+	// Use this for initialization
+	void Start () {
+		projectPath = Application.dataPath + "/Movie/";
+		print (projectPath);
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnGUI()
+	{
+		if(GUI.Button(new Rect((Screen.width/2)- buttonSize/2, (Screen.height/3)- buttonSize/2, buttonSize, buttonHeight), "Watch Anti-aliasing Movie"))
+		{
+			Application.OpenURL(projectPath + "Aa-Movie_the_final.wmv");
+		}
+
+		if(GUI.Button(new Rect((Screen.width/2)- buttonSize/2, (Screen.height/3)*2- buttonSize/2, buttonSize, buttonHeight), "Proceed with the test"))
+		{
+			print ("Experiment Scene");
+			Application.LoadLevel("Experiment1");
+		}
+	}
+}
