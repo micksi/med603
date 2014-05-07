2defe9fee337a503fcd6a5f3123b7a09ef15607f
Mick Neupart <mickneupart@gmail.com>
2014-05-07 15:10:11 +0200

    added objective system to guide the player around.

---
+++ BootCamp/Assets/ObjectiveDialog.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectiveDialog : MonoBehaviour {
+
+	public string dialog;
+
+	[HideInInspector]
+	public bool currentObjective = false;
+
+	private Rect dialogRect;
+	
+	// Use this for initialization
+	void Start () {
+		dialogRect = new Rect(Screen.width-150, Screen.height/2,140,50);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnGUI()
+	{
+		if(currentObjective)
+		{
+			GUI.Label(dialogRect,dialog);
+		}
+	}
+}
