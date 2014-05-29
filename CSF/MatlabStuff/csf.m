224bcd58ccba9ee3dee24e76c730e4d4a17589ae
Adam Schønemann <adamschoenemann@gmail.com>
2014-05-29 10:24:03 +0200

    small modifications

--- CSF/MatlabStuff/csf.m
+++ CSF/MatlabStuff/csf.m
@@ -1,6 +1,7 @@
 % TODO add optional arguments
+% Important: CT0 is reciprocal, e.g. not 1/64 but just 64
 function result = csf(e, CT0, alpha, e2)
-    nom = e2 * log(CT0);
+    nom = e2 .* log(CT0);
     denom = alpha * (e + e2);
-    result = nom / denom;
+    result = nom ./ denom;
 end
